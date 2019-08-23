39a2475fe8f8c182cc1fca928cb058f74a740c0f
Kristóf Kövér <kristof.kover@lightware.com>
2019-08-23 14:46:35 +0200

    Checkout page and checkout item added

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -11,3 +11,13 @@ export const  selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
 );
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems => cartItems.reduce((accumulatedTotal, cartItem) => accumulatedTotal + cartItem.quantity * cartItem.price, 0)
+);
