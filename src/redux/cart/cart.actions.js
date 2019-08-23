477a810688d7b1102fb9c6a9767d38707f98e240
Kristóf Kövér <kristof.kover@lightware.com>
2019-08-23 15:48:40 +0200

    Increase/decrease item quantity added tot the checkout item

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,18 @@ export const addItem = item => ({
   type: CartActionTypes.ADD_ITEM,
   payload: item
 });
+
+export const clearItemFromCart = item => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item
+});
+
+export const increaseItemQuantity = item => ({
+  type: CartActionTypes.INCREASE_ITEM_QUANTITY,
+  payload: item
+});
+
+export const decreaseItemQuantity = item => ({
+  type: CartActionTypes.DECREASE_ITEM_QUANTITY,
+  payload: item
+});
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -11,3 +11,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+};
+
+export const modifyItemQuantity = (cartItems, cartItemToModify, diff) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToModify.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToModify.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToModify.id
+    ? { ...cartItem, quantity: cartItem.quantity + diff }
+    : cartItem
+  );
+};
