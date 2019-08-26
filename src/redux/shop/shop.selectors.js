13ea5ed5f3dc4a713ae9f2018bffc527f0082b43
Kristóf Kövér <kristof.kover@lightware.com>
2019-08-26 16:07:33 +0200

    Added a new Collectoin page, using match params to  get the collection from the shop reducer

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -6,3 +6,18 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 );
+
+export const selectCollectionsForPreview = createSelector (
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => {
+      // for some reason collections is an object acting like an array and not recognizing the top level property names
+      var correctedId = { hats: 0, sneakers: 1, jackets: 2, womens: 3, mens: 4 }[collectionUrlParam];
+      return collections[correctedId];
+    }
+  );
